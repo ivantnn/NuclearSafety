842d5e17506fd7d15aedf4ccbbb4850df5a5cbd4
ivantnn <106730322+ivantnn@users.noreply.github.com>
2022-06-17 08:19:04 +0200

    Added signature

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ import streamlit as st
 n = open('out2.mp4','rb')
 b = n.read()
 st.video(b)
+
+st.write('Made by Ivan Tonon for $Nuclear$ $Safety$ course final assessment.')
+st.write('Professors: Cesar Queral, Eduardo Gallego and Gonzalo Jimenez')
