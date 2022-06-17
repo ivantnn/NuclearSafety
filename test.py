c7cb6916a41019ac500461a806a23adb31750705
ivantnn <106730322+ivantnn@users.noreply.github.com>
2022-06-17 17:09:20 +0200

    Update to JSON

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import json
 
-n = open('out2.mp4','rb')
-b = n.read()
-st.video(b)
+
+def load_lottie(filepath:str):
+	with open(filepath,"r") as f:
+		return json.load(f)
+
+load_file = load_lottie('out.mp4.lottie.json')
+#n = open('out2.mp4','rb')
+#b = n.read()
+#st.video(b)
+st_lottie(load_file,
+	speed=1,
+	reverse=False,
+	quality="high")
+st.write("Made by Ivan Tonon for $Nuclear$ $Safety$ course's final assessment")
+st.write("Professors: Cesar Queral, Eduardo Gallego and Gonzalo Jimenez")
 
 st.write('Made by Ivan Tonon for $Nuclear$ $Safety$ course final assessment.')
 st.write('Professors: Cesar Queral, Eduardo Gallego and Gonzalo Jimenez')
